9943787b36aed505ab3b9f5edc3c45e2f857970f
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-05-19 18:52:47 +0900

    add user-defined models.

--- bennuhp/models.py
+++ bennuhp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish_post(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    # Python Internal Method.
+    def _str__(self):
+        return self.title
+
