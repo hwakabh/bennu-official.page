78f151412c5a2d0361016625f7819494d3a8ac27
hwakabh <hrykwkbys1024@yahoo.co.jp>
2018-05-20 01:56:27 +0900

    set up staticfiles, remove comments in settings.py

--- bennuhp/urls.py
+++ bennuhp/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -8,3 +11,5 @@ urlpatterns = [
     url(r'^lives/', views.lives),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bennuofficial/urls.py
+++ bennuofficial/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from bennuhp.views import page_not_found
 
@@ -8,3 +10,6 @@ urlpatterns = [
     url('^bennu/', include('bennuhp.urls')),
     url(r'^', page_not_found),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
