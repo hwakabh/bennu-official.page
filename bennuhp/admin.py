a4d0a1ca1e0a344c694e95f017ebbbd44bb38292
hwakabh <hrykwkbys1024@gmail.com>
2023-12-18 16:58:41 +0900

    chore(models): regsitered each models as admin app and updated migrations.

--- bennuhp/admin.py
+++ bennuhp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-
+from .models import Music, Movie, LiveSchedule
 
 # To handle in Django administration site
-# admin.site.register()
+admin.site.register(Music)
+admin.site.register(Movie)
+admin.site.register(LiveSchedule)
