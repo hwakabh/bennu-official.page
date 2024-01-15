cf61df47acc4280a26797318467a0197f8feafc1
hwakabh <hrykwkbys1024@gmail.com>
2024-01-16 01:10:36 +0900

    feat: updated LiveSchedules models with adding packages.

--- bennuhp/models/liveschedule.py
+++ bennuhp/models/liveschedule.py
@@ -1,7 +1,23 @@
 import uuid
 
 from django.db import models
-
+from django_mysql.models import ListCharField
 
 class LiveSchedule(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    title = models.CharField(max_length=200)
+    descriptions = models.TextField(null=True)
+    date = models.DateTimeField()
+    venue = models.TextField()
+    url = models.URLField()
+
+    open_time = models.TimeField(null=True)
+    start_time = models.TimeField()
+    price = models.IntegerField()
+    with_actors = ListCharField(
+        base_field=models.CharField(max_length=20)
+    )
+
+    def __str__(self):
+        return self.title
