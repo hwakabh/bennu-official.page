6e0f9e19b779b06ead94512dbe0e30e3e0af5db8
hwakabh <hrykwkbys1024@gmail.com>
2024-07-19 00:35:58 +0900

    refactor(urls): made top page as fallback URL path.

--- bennuhp/urls.py
+++ bennuhp/urls.py
@@ -10,10 +10,10 @@ app_name = 'bennuhp'
 
 urlpatterns = [
     path('healthz', views.HealthzView.as_view(), name='healthz'),
-    path('home/', views.AppRootTemplateView.as_view(), name='root'),
     path('biography/', views.BioTemplateView.as_view(), name='bio'),
     path('discography/', views.DiscoListView.as_view(), name='disco'),
     path('lives/', views.LivesListView.as_view(), name='live'),
+    path('', views.AppRootTemplateView.as_view(), name='root'),
 ]
 
 if settings.DEBUG:
